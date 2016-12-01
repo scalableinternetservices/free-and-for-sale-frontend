e0d1d3378515df8631be28eb854e32b50edbc46e
fumu <332853786@qq.com>
2016-11-30 17:13:04 -0800

    change servere address

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // export var serverAddress = 'https://35.163.37.110/';
-export var serverAddress = 'http://35.165.1.161/'
+export var serverAddress = 'http://35.163.124.214/'
 // export var serverAddress = 'http://localhost:3000/';
